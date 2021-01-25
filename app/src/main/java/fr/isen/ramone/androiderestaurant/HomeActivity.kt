f67ce2907f24fed8d87f6b1a5eed58cc8e9556b4
enzo.ramone <enzo.ramone@isen.yncrea.fr>
2021-01-25 12:03:11 +0100

    second commit

--- app/src/main/java/fr/isen/ramone/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/ramone/androiderestaurant/HomeActivity.kt
@@ -2,10 +2,18 @@ package fr.isen.ramone.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.isen.ramone.androiderestaurant.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.starter.setOnClickListener {
+        }
     }
 }
